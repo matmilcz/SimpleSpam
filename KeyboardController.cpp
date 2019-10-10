644fc2494feef13d32603ccebe931047cd809e10
matmilcz <matmilcz@gmail.com>
2019-10-10 15:48:34 +0200

    fix warnings for GlobalLock and memcpy

--- KeyboardController.cpp
+++ KeyboardController.cpp
@@ -27,8 +27,22 @@ void KeyboardController::ctrlC(const std::string& stringToCopy)
 	const char* output = stringToCopy.c_str();
 	const size_t len = strlen(output) + 1;
 	HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, len);
-	memcpy(GlobalLock(hMem), output, len);
+
+	if (hMem == nullptr)
+	{
+		return;
+	}
+
+	LPVOID hMemPtr = GlobalLock(hMem);
+
+	if (hMemPtr == nullptr)
+	{
+		return;
+	}
+
+	memcpy(hMemPtr, output, len);
 	GlobalUnlock(hMem);
+
 	OpenClipboard(0);
 	EmptyClipboard();
 	SetClipboardData(CF_TEXT, hMem);
